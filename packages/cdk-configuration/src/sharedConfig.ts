2c75a659f1ab512205f2329a17d1f705d58f085d
axelf <axelf@theodo.fr>
2023-09-15 16:00:06 +0200

    Initial commit from Create Swarmion App

---
+++ packages/cdk-configuration/src/sharedConfig.ts
@@ -0,0 +1,6 @@
+import { swarmionCdkEsbuildConfig } from '@swarmion/serverless-helpers';
+
+export const projectName = 'cdk-app-sync-test';
+export const region = 'eu-west-1';
+
+export const sharedLambdaEsbuildConfig = swarmionCdkEsbuildConfig;
